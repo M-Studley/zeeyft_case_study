02fa9297dc19c701d614e0163a4b4902e12bf959
Michael Studley <michael.r.studley@gmail.com>
2024-07-18 20:47:11 +0700

    CREATED:   - None UPDATED:   - get_target_data() to focus on selenium   - query_google() to focus on selenium DELETED:   - google_html_parser() too difficult to get desired data recursively

--- app/google_places/google_maps.py
+++ app/google_places/google_maps.py
@@ -1,8 +1,9 @@
 from time import sleep
 from random import random
-
-from bs4 import BeautifulSoup
-import bs4
+# from typing import List, Dict
+#
+# from bs4 import BeautifulSoup
+# import bs4
 import selenium
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
@@ -40,7 +41,7 @@ def init_chrome_web_driver() -> webdriver:
         webdriver.Chrome: Configured Chrome WebDriver instance.
     """
     options = ChromeOptions()
-    options.add_argument("--headless=new")
+    # options.add_argument("--headless=new")
     options.add_argument("--lang=en-US")
     options.add_argument("--start-maximized")
     driver = webdriver.Chrome(options=options)
@@ -73,47 +74,62 @@ def scroll_results_window(driver: selenium.webdriver.Chrome) -> None:
             print('Scrolling Result Window...')
 
 
-def google_html_parser(driver: selenium.webdriver.Chrome) -> list[str]:
+# def google_html_parser(driver: selenium.webdriver.Chrome) -> list[str]:
+#     """
+#     Parses the HTML of the current page loaded in the WebDriver and extracts target data.
+#
+#     Args:
+#         driver (selenium.webdriver.Chrome): The WebDriver instance currently in use.
+#
+#     Returns:
+#         list[str]: A list of extracted target data from the search results.
+#     """
+#     page_source = driver.page_source
+#     tree = BeautifulSoup(page_source, 'html.parser')
+#     print('Parsing HTML...')
+#     return get_target_data(tree)
+
+
+def get_target_data(driver: selenium.webdriver.Chrome) -> list[dict[str, str]]:
     """
-    Parses the HTML of the current page loaded in the WebDriver and extracts target data.
+    Extracts target data (e.g., company names) from the parsed HTML elements.
 
     Args:
         driver (selenium.webdriver.Chrome): The WebDriver instance currently in use.
 
     Returns:
-        list[str]: A list of extracted target data from the search results.
-    """
-    page_source = driver.page_source
-    tree = BeautifulSoup(page_source, 'html.parser')
-    print('Parsing HTML...')
-    return get_target_data(tree)
-
-
-def get_target_data(result_elements: bs4.BeautifulSoup) -> list[str]:
-    """
-    Extracts target data (e.g., company names) from the parsed HTML elements.
-
-    Args:
-        result_elements (bs4.BeautifulSoup): Parsed HTML elements.
-
-    Returns:
         list[str]: A list of target data (e.g., company names).
     """
-    company_names = []
-    company_phone = []
-    company_links = []
-    name_tags = result_elements.find_all('div', {'class': 'qBF1Pd fontHeadlineSmall'})
-    phone_tags = result_elements.find_all('span', {'class': 'UsdlK'})
-    website_tags = result_elements.find_all('a', {'class': 'lcr4fd S9kvJb'})
+    # company_names = []
+    # company_phone = []
+    # company_links = []
 
-    for element in name_tags:
-        company_names.append(element.get_text())
-    for element in phone_tags:
-        company_phone.append(element.get_text())
-    for element in website_tags:
-        company_links.append(element.get('href'))
+    elements = driver.find_elements(By.CSS_SELECTOR, value='div[role="feed"] > div > div[jsaction]')
 
-    return company_names
+    results = []
+    for element in elements:
+        target_data = {}
+
+        try:
+            target_data['title'] = element.find_element(By.CSS_SELECTOR, value='.fontHeadlineSmall').text
+        except Exception:
+            pass
+
+        if target_data.get('title'):
+            results.append(target_data)
+
+    # name_tags = result_elements.find_all('div', {'class': 'fontHeadlineSmall'})
+    # phone_tags = result_elements.find_all('span', {'class': 'UsdlK'})
+    # website_tags = result_elements.find_all('a', {'class': 'lcr4fd S9kvJb'})
+    #
+    # for element in name_tags:
+    #     company_names.append(element.get_text())
+    # for element in phone_tags:
+    #     company_phone.append(element.get_text())
+    # for element in website_tags:
+    #     company_links.append(element.get('href'))
+
+    return results
 
 
 def query_google(driver: webdriver, url: str):
@@ -128,13 +144,13 @@ def query_google(driver: webdriver, url: str):
         list[str]: A list of extracted target data from the search results.
     """
     driver.get(url)
-    # language_selector(driver)
-    sleep(200 + random())
+    # language_selector(driver)  # ONLY USE WITH NON-HEADLESS
+    sleep(1 + random())
     scroll_results_window(driver)
     sleep(10 + random())
-    result_set = google_html_parser(driver)
+    # result_set = google_html_parser(driver)
     sleep(1 + random())
-    return result_set
+    return get_target_data(driver)  # result_set
 
 
 def get_all_country_url(url: str, countries: list[str]) -> list[list[str]]:
@@ -148,19 +164,21 @@ def get_all_country_url(url: str, countries: list[str]) -> list[list[str]]:
     Returns:
         list[list[str]]: A list of lists containing extracted target data from the search results for each country.
     """
-    all_results = []
-    driver = init_chrome_web_driver()
-    for country in countries:
-        modified_url = url+country
-        all_results.append(query_google(driver, modified_url))
+    try:
+        all_results = []
+        driver = init_chrome_web_driver()
+        for country in countries:
+            modified_url = url+country
+            all_results.append(query_google(driver, modified_url))
 
-    print('Shutting down WebDriver session...')
-    driver.quit()
+        print('Shutting down WebDriver session...')
+    finally:
+        driver.quit()
 
     return all_results
 
 
 if __name__ == '__main__':
-    results = get_all_country_url(search_engine, countries=['australia'])
-    for result in results:
+    all_results = get_all_country_url(search_engine, countries=['australia'])
+    for result in all_results:
         print(result)
