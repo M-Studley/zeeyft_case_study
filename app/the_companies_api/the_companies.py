14c5e067255883698c6f2f70d9ccdbcbd959faf1
Michael Studley <michael.r.studley@gmail.com>
2024-07-20 21:28:04 +0700

    CREATED:   - None UPDATED:   - None DELETED:   - None

--- app/the_companies_api/the_companies.py
+++ app/the_companies_api/the_companies.py
@@ -1,6 +1,7 @@
 from time import sleep
 from random import random, randint
 
+import pandas as pd
 import selenium
 from selenium import webdriver
 from selenium.webdriver import ChromeOptions
@@ -209,8 +210,11 @@ def add_new_condition_manager(driver: selenium.webdriver.Chrome) -> None:
 
 def data_extraction(driver: selenium.webdriver.Chrome) -> list[dict]:
     print('Attempting Data Extraction...')
-    elements = driver.find_elements(By.CSS_SELECTOR, value='.layout-content-blocker.relative')
-    root_xpath = '//*[@id="__nuxt"]/div[2]/main/div[1]/div/div/div[2]/div[2]/div/div[1]/div[2]/'
+    root_selector = ('#__nuxt > div.layout-main.bg-gray-100.bg-repeat.text-gray-900.leading-relaxed.antialiased > '
+                     'main > div.page-companies.lg\:min-h-3xl.lg\:flex.min-h-screen > div > div > '
+                     'div.layout-content-blocker.relative')
+    elements = driver.find_elements(By.CSS_SELECTOR, value=root_selector)
+    print(elements)
 
     results = []
     for element in elements:
@@ -219,56 +223,69 @@ def data_extraction(driver: selenium.webdriver.Chrome) -> list[dict]:
         try:
             target_data['title'] = element.find_element(
                 By.XPATH,
-                value=f'{root_xpath}span[2]/div/div/span').text
+                value=f'{root_selector} > div.z-20 > div > div.flex-1.overflow-x-auto.whitespace-nowrap > '
+                      f'div:nth-child(2) > span:nth-child(2) > div > div > span').text
         except Exception:
             target_data['title'] = 'None'
 
         try:
             target_data['url'] = element.find_element(
                 By.XPATH,
-                value=f'{root_xpath}span[2]/div/a').get_attribute('href')
+                value=f'{root_selector} > div.z-20 > div > div.flex-1.overflow-x-auto.whitespace-nowrap > '
+                      f'div:nth-child(2) > span:nth-child(2) > div > a').get_attribute('href')
         except Exception:
             target_data['url'] = 'None'
 
         try:
             target_data['revenue'] = element.find_element(
                 By.XPATH,
-                value=f'{root_xpath}span[4]/span/span').text
+                value=f'{root_selector} > div.z-20 > div > div.flex-1.overflow-x-auto.whitespace-nowrap > '
+                      f'div.mb-3.h-6.flex.items-center.text-xs.font-semibold.uppercase > '
+                      f'span.flex.select-none.items-center.tracking-tight.flex-initial.justify-start.px-6.w-56 > '
+                      f'div.text-xs > span').text
         except Exception:
             target_data['revenue'] = 'None'
 
         try:
             target_data['employees'] = element.find_element(
                 By.XPATH,
-                value=f'{root_xpath}span[5]/span/span').text
+                value=f'{root_selector} > div.z-20 > div > div.flex-1.overflow-x-auto.whitespace-nowrap > '
+                      f'div.mb-3.h-6.flex.items-center.text-xs.font-semibold.uppercase > span:nth-child(5) > '
+                      f'div.text-xs > span').text
         except Exception:
             target_data['employees'] = 'None'
 
         try:
             target_data['country'] = element.find_element(
                 By.XPATH,
-                value=f'{root_xpath}span[7]/div/div[1]/div[2]/span').text
+                value=f'{root_selector} > div.z-20 > div > div.flex-1.overflow-x-auto.whitespace-nowrap > '
+                      f'div:nth-child(2) > span:nth-child(7) > div > div.base-flag.flex.items-center.flex-row > '
+                      f'div.uppercase.font-medium.ml-1\\.5.text-xs.flex-1 > span').text
         except Exception:
             target_data['country'] = 'None'
 
         try:
             target_data['region'] = element.find_element(
                 By.XPATH,
-                value=f'{root_xpath}span[8]/div/div[1]/span').text
+                value=f'{root_selector} > div.z-20 > div > div.flex-1.overflow-x-auto.whitespace-nowrap > '
+                      f'div:nth-child(2) > span:nth-child(8) > div > div.uppercase.font-medium.text-xs > span').text
         except Exception:
             target_data['region'] = 'None'
 
         try:
             target_data['city,state'] = element.find_element(
                 By.XPATH,
-                value=f'{root_xpath}span[8]/div/div[2]/div/span').text
+                value=f'{root_selector} > div.z-20 > div > div.flex-1.overflow-x-auto.whitespace-nowrap > '
+                      f'div:nth-child(2) > span:nth-child(8) > div > '
+                      f'div.base-icon.flex.flex-row.items-center.mt-1\\.5 > div > span').text
         except Exception:
             target_data['city,state'] = 'None'
 
         try:
             target_data['linkedin_url'] = element.find_element(
                 By.XPATH,
-                value=f'{root_xpath}span[10]/div/div[1]/a').get_attribute('href')
+                value=f'{root_selector} > div.z-20 > div > div.flex-1.overflow-x-auto.whitespace-nowrap > '
+                      f'div:nth-child(2) > span:nth-child(10) > div > div:nth-child(1) > a').get_attribute('href')
         except Exception:
             target_data['linkedin_url'] = 'None'
 
@@ -316,8 +333,8 @@ def main():
         except Exception as e:
             print(f'Data Extraction Failed! Exception: {e}')
 
-        for result in results:  # noqa
-            print(result)
+        df = pd.DataFrame(results)
+        df.to_csv("Wine_Distributors.csv")
 
     finally:
         print('Shutting Down WebDriver Session...')
